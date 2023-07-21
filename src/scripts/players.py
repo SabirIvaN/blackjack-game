3c8232a11f79aa7ce52643a8802d21eeef61919e
Иван Сабиров <sabirivan@yandex.com>
2023-07-21 20:53:35 +0600

    Update files

--- src/scripts/players.py
+++ src/scripts/players.py
@@ -3,6 +3,7 @@ def player_busts(player, dealer, chips):
     print("Player busts!")
     chips.lose_bet()
 
+# The player_busts function gives the player a win
 def player_wins(player, dealer, chips):
     print("Player wins!")
     chips.win_bet()
